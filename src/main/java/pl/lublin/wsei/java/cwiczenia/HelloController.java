2a5e5ec186a8ccd9b7950f2009aee5c5b073455c
Lukmajcher <100759817+LMajcherWsei@users.noreply.github.com>
2022-06-11 20:29:22 +0200

    Lab3b. Punkt 3 i 4. Dodano AnchorPane, Button, Label i ListView. Dodano funkcjonalności wybrania pliku oraz wyświetlenia ścieżki bezwzględnej tego pliku dla elementu Label oraz wyświetlenie tytułów Infografik w ListView.

--- src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/HelloController.java
@@ -1,14 +1,41 @@
 package pl.lublin.wsei.java.cwiczenia;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+    public Label lbFile;
+
+    FileChooser fileChooser = new FileChooser();
+    FileChooser.ExtensionFilter xmlFilter =  new FileChooser.ExtensionFilter("Pliki XML (*.xml)", "*.xml");
+
+    public ListView lstInfografiki;
+    ObservableList<String> tytuly = FXCollections.observableArrayList();
+    GusInfoGraphicList igList;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize(){
+        fileChooser.getExtensionFilters().add(xmlFilter);
+    }
+
+    public void btnOpenFileAction(ActionEvent actionEvent) {
+        File file = fileChooser.showOpenDialog(null);
+        if(file != null){
+            igList = new GusInfoGraphicList(file.getAbsolutePath());
+            lbFile.setText(file.getAbsolutePath());
+            for (Infografika ig: igList.infografiki) tytuly.add(ig.tytul);
+            lstInfografiki.setItems(tytuly);
+        }
+        else {
+            lbFile.setText("Proszę wczytać plik ...");
+        }
     }
 }
